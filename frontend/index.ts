76ece2b66fc58de45f62db3ea2fc68b567ded1fc
Olli Tietäväinen <ollit@vaadin.com>
2021-11-18 09:48:48 +0200

    initial commit

---
+++ frontend/index.ts
@@ -0,0 +1,6 @@
+import { Router } from '@vaadin/router';
+export const router = new Router(document.querySelector('#outlet'));
+
+router.setRoutes([]);
+
+
---
+++ src/main/java/com/vaadin/training/data/endpoint/DataEndpoint.java
@@ -0,0 +1,21 @@
+package com.vaadin.training.data.endpoint;
+
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.fusion.Endpoint;
+import com.vaadin.training.data.service.ValueService;
+
+@Endpoint
+@AnonymousAllowed
+public class DataEndpoint {
+
+    private final ValueService valueService;
+
+    public DataEndpoint(ValueService valueService) {
+        this.valueService = valueService;
+    }
+
+    public String getValue() {
+        return valueService.getValue();
+    }
+
+}
